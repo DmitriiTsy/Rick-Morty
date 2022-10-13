f4525a6a9c69f247c857cd24a1d50cfd27508940
Darcless1 <dmitry.tsyplakov.works@gmail.com>
2022-10-13 13:25:22 -0700

    functionality-update

--- src/components/Characters/Character.js
+++ src/components/Characters/Character.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import './Character.css'
 
 const Character = ({props, index}) => {
-    const {image,name,status,species,gender} = props
+    const {image,name,status,species,gender, episode} = props
 
     const [TouchImage, setTouchImage] = useState(false)
 
@@ -10,6 +10,8 @@ const Character = ({props, index}) => {
         event.preventDefault();
         return TouchImage === false? setTouchImage(true) : setTouchImage(false)
     }
+
+    const episodes = episode.length
     return (
         <div className='Character-wrapper'>
             <div className="Character-wrapper__images">
@@ -17,12 +19,36 @@ const Character = ({props, index}) => {
             </div>
             {TouchImage && 
             <div className="Character-wrapper__text">
-                <p key={index}>Name: {name}</p>
-                <p>Status: {status}</p>
-                <p>Species:{species}</p>
-                <p>Gender:{gender}</p>
-                <p>Origin name:{props.origin.name}</p>
-                <p>Location:{props.location.name}</p>
+                <div key={index} className="Character-wrapper__name">{name}</div>
+                <div className='Character-wrapper__species-block'>
+                    <div>
+                        {species}
+                    </div>
+                    <div className='Character-wrapper__species-wrapper'>
+                        <div className='Character-wrapper__indicator' style={status === 'Alive'? {backgroundColor: 'green'}: {backgroundColor: 'red'}}></div>
+                        <div>({status})</div>
+                    </div>
+                </div>
+                <div className='Character-wrapper-gender'>
+                    <div className='Character-wrapper-gender__text'>Gender: </div>
+                    {gender}
+                </div>
+                <div className='Character-wrapper-seen'>
+                    <div className='Character-wrapper-seen__text'>First seen in: </div>
+                    {props.origin.name}
+                </div>
+                <div className='Character-wrapper-location'>
+                    <div className='Character-wrapper-location__text'>Last known location: </div>
+                    {props.location.name}
+                </div>
+                <div className='Character-wrapper-episodes'>
+                    <div className='Character-wrapper-episodes__text'>
+                        Episodes: 
+                    </div>
+                    <div>
+                        {episodes}
+                    </div>
+                </div>
             </div>}
         </div>
     )
--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import Character from '../Characters/Character.js'
 import './Characters.css'
+import FilterIcon from '../../content/icons/filter.png'
 
 
 const Characters = () => {
@@ -10,7 +11,17 @@ const Characters = () => {
     const [search, setSearch] =useState('');
     const [page, setPage] = useState(1);
 
-    const [error, setError] =useState(false);
+    const [filter, setFilter] = useState(false)
+
+    const [status, setStatus] = useState(false);     //States to open additional filters
+    const [gender, setGender] = useState(false);
+    const [species, setSpecies] = useState(false);
+
+    const [currentStatus, setCurrentStatus] = useState('');         //States with value
+    const [currentGender, setCurrentGender] = useState('');
+    const [currentSpecies, setCurrentSpecies] = useState('');
+
+    const [error, setError] = useState(false);
   
     useEffect(() => {                   //Checking some data objects (testing purposes) 
       console.log('url: ', url)
@@ -20,7 +31,7 @@ const Characters = () => {
     }, [info, url,search, results])
   
     useEffect(()=>{                     //combined url + result in input for search filter purpose
-        fetch(`${url}?page=${page}&name=${search}`, {
+        fetch(`${url}?page=${page}&name=${search}&status=${currentStatus}&gender=${currentGender}&species=${currentSpecies}`, {
           method: 'GET'
         }).then((response) => {
             if (response.status === 404) {
@@ -37,7 +48,7 @@ const Characters = () => {
             setPage(1)
             console.log(error)
         })
-    }, [search, page])                       //Serach as a main dependency for this 
+    }, [search, page, url, currentStatus, currentGender, currentSpecies])                       //Serach as a main dependency for this 
 
     const HandlerBefore = (event) => {
         event.preventDefault();
@@ -63,23 +74,115 @@ const Characters = () => {
         </div>
     ))
 
+    const HandlerFilter = (event) => {
+        event.preventDefault();
+        return filter === false? setFilter(true): setFilter(false);
+    }
+
+    const HandlerStatus = (event) => {
+        event.preventDefault();
+        return status === false? setStatus(true): setStatus(false);
+    }
+
+    const HandlerSpecies = (event) => {
+        event.preventDefault();
+        return species === false? setSpecies(true): setSpecies(false);
+    }
+
+    const HandlerGender = (event) => {
+        event.preventDefault();
+        return gender === false? setGender(true) : setGender(false)
+    }
+
+    const HandlerCurrentStatus = (event) => {
+        event.preventDefault();
+        setCurrentStatus(event.target.value)
+    }
+
+    const HandlerCurrentSpecies = (event) => {
+        event.preventDefault();
+        setCurrentSpecies(event.target.value)
+    }
+
+    const HandlerCurrentGender = (event) => {
+        event.preventDefault();
+        setCurrentGender(event.target.value)
+    }
+
+    const HandlerClean = (event) => {
+        event.preventDefault();
+        setSearch('')
+        setStatus(false)
+        setSpecies(false)
+        setGender(false)
+        setCurrentStatus('')
+        setCurrentGender('')
+        setCurrentSpecies('')
+        setError(false)
+    }
+
     return (
         <div className="all-wrapper">
-            <button onClick={(event) => HandlerBefore(event)}>-</button>
-            <p>{page}/{info.pages}</p>
-            <button onClick={(event) => HandlerAfter(event)}>+</button>
-
-            <div>
+            <div className="all-wrapper-input">
+                <button className="all-wrapper-input__filter-button" onClick={HandlerFilter}>
+                    <img src={FilterIcon} className="all-wrapper-input__filter"/>
+                </button>
                 <input onChange={(e) => {
                 setSearch(e.target.value)
-            }}
-            value={search}
-            type='text'
-            placeholder='Enter name'
-            /> 
+                }}
+                value={search}
+                type='text'
+                placeholder='Enter name'
+                /> 
             </div>
             <div className='elements-wrapper'>
+            {error && <div>No characters found</div>}
+            {filter && 
+            <div className='filter-wrapper'>
+                <button onClick={HandlerClean}>Clear Filters</button>
+                <div className='filter-buttons-wrapper'>
+                    <button onClick={HandlerStatus}>Status</button>
+                    {status && 
+                    <div>
+                        <button onClick={HandlerCurrentStatus} value='Alive'>Alive</button>
+                        <button onClick={HandlerCurrentStatus} value='Dead'>Dead</button>
+                        <button onClick={HandlerCurrentStatus} value='Unknown'>Unknown</button>
+                    </div>
+                    }
+                    <button onClick={HandlerGender}>Gender</button>
+                    {gender && 
+                    <div>
+                        <button onClick={HandlerCurrentGender} value='female'>female</button>
+                        <button onClick={HandlerCurrentGender} value='male'>male</button>
+                        <button onClick={HandlerCurrentGender} value='unknown'>unknown</button>
+                        <button onClick={HandlerCurrentGender} value='genderless'>genderless</button>
+                    </div>
+                    }
+                    <button onClick={HandlerSpecies}>Species</button>
+                    {species && 
+                    <div>
+                        <button onClick={HandlerCurrentSpecies} value='Human'>Human</button>
+                        <button onClick={HandlerCurrentSpecies} value='Alien'>Alien</button>
+                        <button onClick={HandlerCurrentSpecies} value='Humanoid'>Humanoid</button>
+                        <button onClick={HandlerCurrentSpecies} value='Poopybutthole'>Poopybutthole</button>
+                        <button onClick={HandlerCurrentSpecies} value='Mythological'>Mythological</button>
+                        <button onClick={HandlerCurrentSpecies} value='Unknown'>Unknown</button>
+                        <button onClick={HandlerCurrentSpecies} value='Animal'>Animal</button>
+                        <button onClick={HandlerCurrentSpecies} value='Disease'>Disease</button>
+                        <button onClick={HandlerCurrentSpecies} value='Robot'>Robot</button>
+                        <button onClick={HandlerCurrentSpecies} value='Cronenberg'>Cronenberg</button>
+                        <button onClick={HandlerCurrentSpecies} value='Planet'>Planet</button>
+                    </div>
+                    }
+                </div>
+            </div>
+            }
             {FilteredArray}
+            </div>
+            <div className='all-pages'>
+                <button onClick={(event) => HandlerBefore(event)}>-</button>
+                <p>{page}/{info.pages}</p>
+                <button onClick={(event) => HandlerAfter(event)}>+</button>
             </div>
         </div>
     )
